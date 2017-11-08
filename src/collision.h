eb7f3dcab5d77fcfcbad75ea97d8ac2c16c6deba
honghu xu <honghu.xv@gmail.com>
2017-11-08 15:15:54 +0800

    feat: collision identifier
    new feature: identify collision to specific lane.

---
+++ src/collision.h
@@ -0,0 +1,20 @@
+#ifndef PATH_PLANNING_PREDICTION_H
+#define PATH_PLANNING_PREDICTION_H
+
+#include <vector>
+
+using namespace::std;
+
+class collision {
+
+public:
+  bool iscollision(int lane, vector<double> car_state);
+
+  // bool all_lane_collision = false;
+  // int path_size = 50;
+  // double end_path_s;
+
+};
+
+
+#endif //PATH_PLANNING_PREDICTION_H
